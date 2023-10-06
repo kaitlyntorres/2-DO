4cbb11211b704bf58b0327ad6f6fc6ff4d4b7a99
Kaitlyn Torres <71412255+kaitlyntorres@users.noreply.github.com>
2023-10-06 15:01:58 -0400

    Add files via upload
    Can successfully delete tasks

---
+++ 2-DO-main/Website Code/website/views.py
@@ -0,0 +1,46 @@
+from flask import Blueprint, render_template, request, flash,redirect, url_for
+from flask_login import login_required, current_user
+from .models import Task
+from . import db
+import json
+import jsonify
+
+
+#This application has a bunch of URLs defined here so that we can have our views defined in several files and not a single one
+#This is what Blueprints allows us to do
+views = Blueprint('views', __name__)
+
+#Decorator, calls on the function when this route is hit
+@views.route('/', methods = ['GET', 'POST'])
+@login_required
+def home():
+    if request.method == "POST":
+        title = request.form.get('title')
+        description = request.form.get('description')
+        date = request.form.get('date')
+        tag = request.form.get('tag')
+        priority = request.form.get('priority')
+
+
+        ## NEED TO ADD NEW TASK TO DATABASE
+        # EXAMPLE IS IN AUTH.PY, ALSO LOOK AT OLD VIEWS.PY ON CSC 530 SUPPORT TICKET WEBSITE GITHUB
+        new_task = Task(title=title,user_id=current_user.id,description=description,due_date=date,tag=tag,priority=priority)
+        db.session.add(new_task)
+        db.session.commit()
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/delete_task/<t>')
+@login_required
+def delete_task(t):
+    task = Task.query.filter_by(id=t).first()
+    if task:
+        msg_text = '%s successfully removed' % str(task)
+        db.session.delete(task)
+        db.session.commit()
+        flash(msg_text)
+    return redirect(url_for('views.home'))
+
+
+
+
