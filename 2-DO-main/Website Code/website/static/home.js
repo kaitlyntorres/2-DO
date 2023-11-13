b224a4f8990d70b815121e0f2aad57c1f6bcbb9f
kurtmuller-studnet <65182311+kcm2019@users.noreply.github.com>
2023-11-13 16:32:46 -0500

    changes

--- 2-DO-main/Website Code/website/static/home.js
+++ 2-DO-main/Website Code/website/static/home.js
@@ -112,11 +112,11 @@ function changeSearchColumn() {
     searchTasks();
 }
 
-function searchTasks() {
+function searchTasks(tableId) {
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("taskSearch");
     filter = input.value.toUpperCase();
-    table = document.getElementById("task-table1");
+    table = document.getElementById(tableId);
     tr = table.getElementsByTagName("tr");
 
     for (i = 0; i < tr.length; i++) {
