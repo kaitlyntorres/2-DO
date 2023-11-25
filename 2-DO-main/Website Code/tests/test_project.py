3c0860323ae3b836046d5cc0e069c3ce898aefd6
kurtmuller-studnet <65182311+kcm2019@users.noreply.github.com>
2023-11-24 23:01:52 -0500

    Sprint 4 Testing Complete
    - Completed the code for Sprint 4 functions
    - Fixed previous tests that were broken from changes

--- 2-DO-main/Website Code/tests/test_project.py
+++ 2-DO-main/Website Code/tests/test_project.py
@@ -8,6 +8,10 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options
 import time
 
 """ *** TEST CASES *** """
@@ -190,7 +194,7 @@ def test_sorting(client, app):
         from bs4 import BeautifulSoup
 
         soup = BeautifulSoup(html_content, "html.parser")
-        table = soup.find("table", {"id": "task-table"})
+        table = soup.find("table", {"id": "task-table1"})
         rows = table.find("tbody").find_all("tr", recursive=False)
 
         # Sort rows based on the specified column
@@ -228,10 +232,52 @@ def test_sorting(client, app):
     expected_sorted_rows = extract_and_sort_rows(response.data.decode("utf-8"), sort_column=6)
     assert sorted_rows == expected_sorted_rows
 
+    def extract_and_sort_rows(html_content, sort_column):
+        from bs4 import BeautifulSoup
+
+        soup = BeautifulSoup(html_content, "html.parser")
+        table = soup.find("table", {"id": "task-table2"})
+        rows = table.find("tbody").find_all("tr", recursive=False)
+
+        # Sort rows based on the specified column
+        rows.sort(key=lambda row: row.find_all("td")[sort_column].text)
+
+        return rows
+    
+    # Test sorting by ID (column index 0)
+    sorted_rows = extract_and_sort_rows(response.data.decode("utf-8"), sort_column=0)
+    expected_sorted_rows = extract_and_sort_rows(response.data.decode("utf-8"), sort_column=0)
+    assert sorted_rows == expected_sorted_rows
+
+    # Test sorting by 
+    sorted_rows = extract_and_sort_rows(response.data.decode("utf-8"), sort_column=1)
+    expected_sorted_rows = extract_and_sort_rows(response.data.decode("utf-8"), sort_column=1)
+    assert sorted_rows == expected_sorted_rows
+
+    # Test sorting by Title (column index 2)
+    sorted_rows = extract_and_sort_rows(response.data.decode("utf-8"), sort_column=2)
+    expected_sorted_rows = extract_and_sort_rows(response.data.decode("utf-8"), sort_column=2)
+    assert sorted_rows == expected_sorted_rows
+
+    # Test sorting by Tag (column index 4)
+    sorted_rows = extract_and_sort_rows(response.data.decode("utf-8"), sort_column=4)
+    expected_sorted_rows = extract_and_sort_rows(response.data.decode("utf-8"), sort_column=4)
+    assert sorted_rows == expected_sorted_rows
+
+    # Test sorting by Priority (column index 5)
+    sorted_rows = extract_and_sort_rows(response.data.decode("utf-8"), sort_column=5)
+    expected_sorted_rows = extract_and_sort_rows(response.data.decode("utf-8"), sort_column=5)
+    assert sorted_rows == expected_sorted_rows
+
+    # Test sorting by Status (column index 6)
+    sorted_rows = extract_and_sort_rows(response.data.decode("utf-8"), sort_column=6)
+    expected_sorted_rows = extract_and_sort_rows(response.data.decode("utf-8"), sort_column=6)
+    assert sorted_rows == expected_sorted_rows
+
 """ *** Sprint 3 Test Cases *** """
 
-# T-07 Ensure that a user can filter by column values
-def test_search_tasks(client, app):
+# T-07 Ensure that a user can filter by column values in the incomplete table
+def test_incomplete_search_tasks(client, app):
     # SHOULD HAVE THIS TEST INPUT A TASK AND THEN CHECK INSTEAD OF RELYING 
     # UPON HAVING THE TESTS ALREADY EXISTING IN THE DATABASE
 
@@ -267,14 +313,14 @@ def test_search_tasks(client, app):
     column_select.send_keys(search_column)
 
     # Find and input the search text
-    task_search = driver.find_element(By.ID, "taskSearch")
+    task_search = driver.find_element(By.ID, "taskSearch1")
     task_search.send_keys(search_text)
     
     # Wait for a moment (you may need to adjust the waiting time)
     driver.implicitly_wait(2)
     
     # Find the tasks table
-    task_table = driver.find_element(By.ID, "task-table")
+    task_table = driver.find_element(By.ID, "task-table1")
 
     # Check if the search results are as expected
     assert f'task1' in task_table.text
@@ -290,12 +336,12 @@ def test_search_tasks(client, app):
     column_select = driver.find_element(By.NAME, "columnSelect")
     column_select.send_keys(search_column)
     time.sleep(1)
-    task_search = driver.find_element(By.ID, "taskSearch")
+    task_search = driver.find_element(By.ID, "taskSearch1")
     task_search.send_keys(search_text)
 
     driver.implicitly_wait(2)
 
-    task_table = driver.find_element(By.ID, "task-table")
+    task_table = driver.find_element(By.ID, "task-table1")
 
     assert f'desc2' in task_table.text
     assert f'desc1' not in task_table.text
@@ -310,12 +356,12 @@ def test_search_tasks(client, app):
     column_select = driver.find_element(By.NAME, "columnSelect")
     column_select.send_keys(search_column)
     time.sleep(1)
-    task_search = driver.find_element(By.ID, "taskSearch")
+    task_search = driver.find_element(By.ID, "taskSearch1")
     task_search.send_keys(search_text)
 
     driver.implicitly_wait(2)
 
-    task_table = driver.find_element(By.ID, "task-table")
+    task_table = driver.find_element(By.ID, "task-table1")
 
     assert f'tag3' in task_table.text
     assert f'tag1' not in task_table.text
@@ -330,12 +376,129 @@ def test_search_tasks(client, app):
     column_select = driver.find_element(By.NAME, "columnSelect")
     column_select.send_keys(search_column)
     time.sleep(1)
-    task_search = driver.find_element(By.ID, "taskSearch")
+    task_search = driver.find_element(By.ID, "taskSearch1")
     task_search.send_keys(search_text)
 
     driver.implicitly_wait(2)
 
-    task_table = driver.find_element(By.ID, "task-table")
+    task_table = driver.find_element(By.ID, "task-table1")
+
+    assert f'High' in task_table.text
+    assert f'Low' not in task_table.text
+    assert f'Medium' not in task_table.text
+
+    # Quit Driver When Done
+    driver.quit() 
+
+# T-07-01 Ensure that a user can filter by column values in the complete table
+# NEED TO CHANGE SO THAT IT SELECTS THE RIGHT "columnSelect" 
+# CURRENT JUST GRABS THE FIRST "columnSelect" AND FINDS THE RIGHT DATA SINCE THE 
+# TABLE WASNT FILTERED OUT, HENCE CONTAINING ALL THE DATA
+def test_complete_search_tasks(client, app):
+    # SHOULD HAVE THIS TEST INPUT A TASK AND THEN CHECK INSTEAD OF RELYING 
+    # UPON HAVING THE TESTS ALREADY EXISTING IN THE DATABASE
+
+   # Set up the GeckoDriver using webdriver_manager
+    gecko_service = Service(GeckoDriverManager().install())
+
+    # Create a WebDriver instance using Firefox and the GeckoDriver
+    driver = webdriver.Firefox(service=gecko_service)
+
+    # Navigate to a website (e.g., Google)
+    driver.get("http://127.0.0.1:3000/login")
+
+    # Find the email and password input fields using WebDriver methods
+    email_field = driver.find_element(By.NAME, "email")
+    password_field = driver.find_element(By.NAME, "password")
+
+    # Enter your login credentials
+    email_field.send_keys("kurt@gmail.com")
+    password_field.send_keys("12345678")
+
+    # Submit the login form
+    password_field.send_keys(Keys.RETURN)
+
+    # Test Title Search 
+    #Search Terms
+    search_column = "title"
+    search_text = "task1complete"
+
+    time.sleep(3)
+
+    # Find and select the search column dropdown
+    column_select = driver.find_element(By.NAME, "columnSelect")
+    column_select.send_keys(search_column)
+
+    # Find and input the search text
+    task_search = driver.find_element(By.ID, "taskSearch2")
+    task_search.send_keys(search_text)
+    
+    # Wait for a moment (you may need to adjust the waiting time)
+    driver.implicitly_wait(2)
+    
+    # Find the tasks table
+    task_table = driver.find_element(By.ID, "task-table2")
+
+    # Check if the search results are as expected
+    assert f'task1' in task_table.text
+    assert f'task2' not in task_table.text
+    assert f'task3' not in task_table.text
+
+    # Test Description Search 
+    driver.refresh()
+    time.sleep(3)
+    search_column = "description"
+    search_text = "desc2complete"
+
+    column_select = driver.find_element(By.NAME, "columnSelect")
+    column_select.send_keys(search_column)
+    time.sleep(1)
+    task_search = driver.find_element(By.ID, "taskSearch2")
+    task_search.send_keys(search_text)
+
+    driver.implicitly_wait(2)
+
+    task_table = driver.find_element(By.ID, "task-table2")
+
+    assert f'desc2' in task_table.text
+    assert f'desc1' not in task_table.text
+    assert f'desc3' not in task_table.text
+
+    # Test Tag Search 
+    driver.refresh()
+    time.sleep(3)
+    search_column = "tag"
+    search_text = "tag3complete"
+
+    column_select = driver.find_element(By.NAME, "columnSelect")
+    column_select.send_keys(search_column)
+    time.sleep(1)
+    task_search = driver.find_element(By.ID, "taskSearch2")
+    task_search.send_keys(search_text)
+
+    driver.implicitly_wait(2)
+
+    task_table = driver.find_element(By.ID, "task-table2")
+
+    assert f'tag3' in task_table.text
+    assert f'tag1' not in task_table.text
+    assert f'tag2' not in task_table.text
+
+    # Test Priority Search 
+    driver.refresh()
+    time.sleep(3)
+    search_column = "priority"
+    search_text = "High"
+
+    column_select = driver.find_element(By.NAME, "columnSelect")
+    column_select.send_keys(search_column)
+    time.sleep(1)
+    task_search = driver.find_element(By.ID, "taskSearch2")
+    task_search.send_keys(search_text)
+
+    driver.implicitly_wait(2)
+
+    task_table = driver.find_element(By.ID, "task-table2")
 
     assert f'High' in task_table.text
     assert f'Low' not in task_table.text
@@ -371,8 +534,8 @@ def test_complete_task(client, app):
         task = Task.query.get(1)
         assert task.status == True # Check that the status changed to True from the False default
 
-
 """*** Sprint 4 Test Cases ***"""
+
 # C-01 Test that a user can view all completed tasks in a separate table
 def test_completed_tasks_views(client, app):
     # Create a user
@@ -446,6 +609,127 @@ def test_remove_reminder_time(client, app):
         assert task.priority == "High"
         assert task.reminder_time == ""
 
+# R-03 Test that the user will receive a reminder notification
+def test_reminder_notification(client, app):
+    # SHOULD HAVE THIS TEST INPUT A TASK AND THEN CHECK INSTEAD OF RELYING 
+    # UPON HAVING THE TESTS ALREADY EXISTING IN THE DATABASE
+
+    # Set up the GeckoDriver using webdriver_manager
+    gecko_service = Service(GeckoDriverManager().install())
+
+    # Create Firefox options
+    firefox_options = Options()
+
+    # Allow notifications
+    firefox_options.set_preference("dom.webnotifications.enabled", True)
+
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference('permissions.default.desktop-notification', 1)
+    #driver = webdriver.Firefox(firefox_profile=profile)
+
+    # Create a WebDriver instance using Firefox and the GeckoDriver
+    driver = webdriver.Firefox(service=gecko_service, options=firefox_options, firefox_profile=profile)
+
+    # Navigate to our site
+    driver.get("http://127.0.0.1:3000/login")
+
+    # Find the email and password input fields using WebDriver methods
+    email_field = driver.find_element(By.NAME, "email")
+    password_field = driver.find_element(By.NAME, "password")
+
+    # Enter your login credentials
+    email_field.send_keys("kurt@gmail.com")
+    password_field.send_keys("12345678")
+
+    # Submit the login form
+    password_field.send_keys(Keys.RETURN)
+
+    time.sleep(3)
+
+    # Test clicking edit button
+    task_table = driver.find_element(By.ID, "task-table1")
+    
+    # Find the edit button in the first row (assuming it's the first row, adjust if needed)
+    edit_button = task_table.find_element(By.XPATH, "//tr[1]//a[contains(@href, '/editform?taskid=')]")
+
+    edit_button.click()
+
+    time.sleep(3)
+
+    # Assuming 'driver' is your WebDriver instance
+    # Find the Reminder time input
+    reminder_time_input = driver.find_element(By.ID, "reminder_time")
+
+    # Get the current time and add one minute
+    current_time = time.localtime()
+    one_minute_later = time.mktime(current_time) + 60
+
+    # Clear the existing value in the input
+    reminder_time_input.clear()
+
+    # Input the new value with the date parts sent one at a time
+    formatted_one_minute_later = time.localtime(one_minute_later)
+
+    # Send the month
+    reminder_time_input.send_keys(time.strftime('%m', formatted_one_minute_later))
+
+    # Send the Tab key
+    #reminder_time_input.send_keys(Keys.TAB)
+    
+    # Send the day
+    reminder_time_input.send_keys(time.strftime('%d', formatted_one_minute_later))
+
+    # Send the Tab key
+    #reminder_time_input.send_keys(Keys.TAB)
+
+     # Send the year
+    reminder_time_input.send_keys(time.strftime('%Y', formatted_one_minute_later))
+
+    # Send the Tab key
+    reminder_time_input.send_keys(Keys.TAB)
+    
+    # Send the hour
+    reminder_time_input.send_keys(time.strftime('%I', formatted_one_minute_later))
+
+    # Send the Tab key
+    #reminder_time_input.send_keys(Keys.TAB)
+
+    # Send the minute
+    reminder_time_input.send_keys(time.strftime('%M', formatted_one_minute_later))
+
+    # Send the space key
+    #reminder_time_input.send_keys(Keys.SPACE)
+
+    # Send the AM/PM indicator
+    reminder_time_input.send_keys(time.strftime('%p', formatted_one_minute_later))
+
+    # Find and click the submit button
+    submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
+    submit_button.click()
+
+    # Wait for the page to reload (adjust wait time as needed)
+    time.sleep(3)
+
+    # Find and click the "Schedule Notification" button for the first task
+    schedule_notification_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//a[contains(@onclick, 'scheduleNotification')]"))
+    )
+    schedule_notification_button.click()
+
+    # Wait for the notification to appear (adjust wait time as needed)
+    time.sleep(1)
+
+    # Wait for the notification to appear (adjust wait time as needed)
+    notification = driver.find_element(By.ID, "notification-container")
+
+    # Perform assertions or other actions as needed
+    assert "Reminder Set!" in notification.text
+
+    # Perform additional checks/assertions as needed
+
+    # Quit the WebDriver
+    driver.quit()
+
 """ *** TESTING ACCESSING HTML VIEWS *** """
 # Test the home page ("/") route
 def test_home(client):
@@ -510,7 +794,7 @@ C-01 Test that a user can view all completed tasks in a separate table
     - Initial set up done, need to implement the rest to finish the testing
 ✅ R-01 Test that the user can set a reminder time
 ✅ R-02 Test that the user can remove the reminder time from a task
-R-03 Test that the user recieves a notificaiton
+✅ R-03 Test that the user recieves a notificaiton
 
 """
 
